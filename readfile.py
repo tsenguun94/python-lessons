710eb98cf647a20abbe646e75617129e9bfe077a
tsenguun94 <kim.tsenguun@yahoo.com>
2017-06-12 09:27:49 +0900

    Add files via upload

---
+++ readfile.py
@@ -0,0 +1,34 @@
+f = open("my_vector.py", "r")
+
+
+
+
+
+
+
+
+# read line or lines
+#print("case 2:")
+#l = f.readlines()
+#print(l)
+
+#print("case 1:")
+#ll = f.read()
+#print(l)
+
+
+
+
+
+
+
+#print lines one by one
+#l1 = f.readline()
+#l2 = f.readline()
+#l3 = f.readline()
+
+#print(l1.strip())
+#print(l2.strip())
+#print(l3.strip())
+
+f.close()
